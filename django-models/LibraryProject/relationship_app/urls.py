ba0c97073a29690ff246599c59bb0210d42a4d77
Gideon Osei Acheampong <gideonz823@gmail.com>
2025-11-09 03:33:33 +0000

    user authentication

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 from .views import list_books
+from django.urls import path
+from . import views
+from django.contrib.auth.views import LoginView, LogoutView
 
 
 urlpatterns = [
