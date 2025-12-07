dca78d76e43ea8085b620c3e4c356b631afe6549
Gideon Osei Acheampong <gideonz823@gmail.com>
2025-12-07 18:20:38 +0000

    Adding Comment Functionality to Blog Posts

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -9,36 +9,28 @@ from .views import (
 app_name = 'blog'
 
 urlpatterns = [
+    # Home
     path('', views.index, name='home'),
-    path('posts/', views.posts, name='posts'),
-    path('post/<int:pk>/', views.post_detail, name='post-detail'),
 
-    # Auth
+    # Simple list of posts (your FBV)
+    path('posts/', views.posts, name='posts'),
+
+    # AUTH
     path('register/', views.register_view, name='register'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
 
-    # Profile
+    # PROFILE
     path('profile/', views.profile_view, name='profile'),
 
-    
-
-    path('', views.post_list, name='post_list'),
-
-    # Create
+    # POST CRUD (Function-Based Views)
     path('post/new/', views.post_create, name='post_create'),
-
-    # Read single post
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
-
-    # Update
     path('post/<int:pk>/update/', views.post_update, name='post_update'),
-
-    # Delete
     path('post/<int:pk>/delete/', views.post_delete, name='post_delete'),
 
-     # Comments
-    path('post/<int:post_id>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
+    # COMMENT CRUD (Checker Required)
+    path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
     path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -123,14 +123,14 @@ class CommentCreateView(LoginRequiredMixin, CreateView):
     form_class = CommentForm
 
     def form_valid(self, form):
-        post = get_object_or_404(Post, pk=self.kwargs['post_id'])
+        post = get_object_or_404(Post, pk=self.kwargs['pk'])
         form.instance.author = self.request.user
         form.instance.post = post
         return super().form_valid(form)
 
     def get_success_url(self):
-        return redirect('post-detail', pk=self.kwargs['post_id']).url
-    
+        return redirect('post-detail', pk=self.kwargs['pk']).url
+
 
 class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Comment
