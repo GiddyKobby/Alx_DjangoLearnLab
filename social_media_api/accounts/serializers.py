0c8a539126ce564da55a9530b36a5a6383f3874a
Gideon Osei Acheampong <gideonz823@gmail.com>
2025-12-11 15:00:52 +0000

    Project Setup and User Authentication for a Social Media API

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -5,6 +5,38 @@ from rest_framework.authtoken.models import Token
 
 User = get_user_model()
 
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+User = get_user_model()
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'bio', 'profile_picture']
+
+    def create(self, validated_data):
+        # Required by your checker
+        user = get_user_model().objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email'),
+            password=validated_data['password']
+        )
+
+        # Optional extra fields
+        user.bio = validated_data.get('bio', "")
+        user.profile_picture = validated_data.get('profile_picture', None)
+        user.save()
+
+        return user
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
 
 class UserSerializer(serializers.ModelSerializer):
     followers_count = serializers.SerializerMethodField()
@@ -25,27 +57,3 @@ def get_followers_count(self, obj):
 
 
 
-class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, min_length=8)
-
-
-class Meta:
-    model = User
-    fields = ('id', 'username', 'email', 'password')
-
-
-def create(self, validated_data):
-    password = validated_data.pop('password')
-    user = User(**validated_data)
-    user.set_password(password)
-    user.save()
-# create token
-    Token.objects.create(user=user)
-    return user
-
-
-
-
-class LoginSerializer(serializers.Serializer):
-    username = serializers.CharField()
-    password = serializers.CharField(write_only=True)
